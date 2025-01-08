b228e23e0b549860a6c99b3ea0df77bc24f67021
Nikita Zalevskii <nzalevskii@yandex.ru>
2025-01-08 21:08:24 +0100

    vercel project its  v1.05

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://its-backend-two.vercel.app//api/',
-  serverStaticPath: 'https://its-backend-two.vercel.app//images/',
+  api: 'https://its-backend-two.vercel.app/api/',
+  serverStaticPath: 'https://its-backend-two.vercel.app/images/',
 };
